747c45127e97c7d40db2c50da0f59a67b78451a3
kamlauka <kamlauka@gmail.com>
2022-10-01 19:12:28 +0200

    todoPage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   }
+//   return <h1>Hello, Stranger.</h1>;
+// }
+
+// const userq = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
+
+
+function TodoPage (props) {
+  <div>
+    <h1>Todo list</h1>
+    {form}
+    CreateTask(
+      id = {testTask.id}
+      descr = {testTask.description}
+    )
+
+  </div>
+
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <TodoPage/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const form = (
+  <form>
+  <textarea>You can write smth</textarea>
+  <input type="submit" value="Create new task" />
+  </form>
+);
+
+const testTask = {
+  id: 1,
+  description: 'ababagalamaga',
+  checked: false,
+}
+
+function CreateTask (props) {
+  return(
+    CheckboxTask (props.id, props.descr)
+  )
+}
+
+
+function CheckboxTask (props) {
+  return (
+    <div className='task'>
+      <input type='checkbox' id={props.id}/>
+      <label for={props.task.id}>{props.taskDescr}</label>
+    </div>
+
+  )
+}
+
+root.render(form)
