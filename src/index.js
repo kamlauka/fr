564eaad8ed14da7a5f630eab8e73eb314e743609
kamlauka <kamlauka@gmail.com>
2022-10-02 17:10:15 +0200

    fixbugs

--- src/index.js
+++ src/index.js
@@ -1,71 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
-// function formatName(user) {
-//   return user.firstName + ' ' + user.lastName;
-// }
-
-// function getGreeting(user) {
-//   if (user) {
-//     return <h1>Hello, {formatName(user)}!</h1>;
-//   }
-//   return <h1>Hello, Stranger.</h1>;
-// }
-
-// const userq = {
-//   firstName: 'Harper',
-//   lastName: 'Perez'
-// };
-
-
-function TodoPage (props) {
-  <div>
-    <h1>Todo list</h1>
-    {form}
-    <CreateTask
-      id = {testTask.id}
-      descr = {testTask.decr}
-    />
-
-  </div>
-
-}
-
-
-
-const form = (
-  <form>
-  <textarea>You can write smth</textarea>
-  <input type="submit" value="Create new task" />
-  </form>
-);
 
 const testTask = {
   id: 1,
   description: 'ababagalamaga',
   checked: false,
+
+  Edit() {},
+  Delete() {},
 }
 
-function CreateTask (props) {
-  return(
-    <CheckboxTask id={props.taskId} descr={props.taskDescr}/>
+function TodoPage(props) {
+  const [taskName, setName] = useState('');
+
+  const myForm = (
+    <form onSubmit={handleSubmit}>
+      <textarea value={taskName} onChange={handleChange}></textarea>
+      <input type="submit" value="Create new task" onClick={CreateTask} />
+    </form>
+  );
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('Hello, world!');
+  }
+
+  function handleChange(e) {
+    console.log(e.target.value);
+  }
+  return (
+    <div>
+      <h1>Todo list</h1>
+      {myForm}
+      <AddCheckbox taskId='1' taskDescr='eat and sleep' taskCompleted='false' />
+    </div>
   )
 }
 
+function CreateTask(props) {
+  if (true) {
+    const newTask = <AddCheckbox taskId={testTask.id} taskDescr={testTask.description} taskCompleted={testTask.checked} />
+    return ( //value textarea
+      console.log(newTask),
+      root.render(
+        { newTask }
+      )
+    )
+  }
+}
 
-function CheckboxTask (props) {
+
+function AddCheckbox(props) {
   return (
     <div className='task'>
-      <input type='checkbox' id={props.id}/>
-      <label for={props.taskId}>{props.taskDescr}</label>
+      <input type='checkbox' id={props.taskId} defaultChecked={props.taskCompleted} />
+      <label htmlFor={props.taskId}>{props.taskDescr}</label>
+      <input type='submit' value='Edit' />
+      <input type='submit' value='Delete' />
     </div>
 
   )
 }
 
 root.render(
-  <TodoPage/>
+  <TodoPage />
 );
